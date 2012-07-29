6128814b971120b81193d3e2425a9752f1c03df9
Gixx <gixx@gixx-web.com>
2012-07-29 08:00:21 +0200

    WH-3 - Documentation fix

--- public/wh_application/module/WebHemi/src/WebHemi/Controller/Website/IndexController.php
+++ public/wh_application/module/WebHemi/src/WebHemi/Controller/Website/IndexController.php
@@ -1,11 +1,23 @@
 <?php
 
 /**
- * Zend Framework (http://framework.zend.com/)
+ * WebHemi
  *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://webhemi.gixx-web.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@gixx-web.com so we can send you a copy immediately.
+ *
+ * @category   WebHemi
+ * @package    WebHemi_Controller_Website
+ * @author     Gixx @ www.gixx-web.com
+ * @copyright  Copyright (c) 2012, Gixx-web (http://www.gixx-web.com)
+ * @license    http://webhemi.gixx-web.com/license/new-bsd   New BSD License
  */
 
 namespace WebHemi\Controller\Website;
