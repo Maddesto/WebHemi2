78d92ee58eacd6ee61f9eeb1fb07f8171320fac8
Gixx <gixx@gixx-web.com>
2013-07-20 22:34:29 +0200

    WH-37 - cleanup comment

--- public/index.php
+++ public/index.php
@@ -1,35 +1,5 @@
 <?php
 
-//$dir = __DIR__ . '/wh_application/vendor/Zend/I18n/Validator/PhoneNumber/';
-//$codes = array();
-//$d = dir($dir);
-//while (false !== ($entry = $d->read())) {
-//	if (strpos($entry, '.php') !== false) {
-//		$data = include_once  $dir . $entry;
-//		$code = $data['code'];
-//		if (isset($codes[$code[0]][$code])) {
-//			$codes[$code[0]][$code] = '-';
-//		}
-//		else {
-//			$codes[$code[0]][$code] = str_replace('.php', '',$entry);
-//		}
-//	}
-//}
-//$d->close();
-//
-//$tmp = array();
-//for ($i = 1; $i <= 9; $i++) {
-//	$tmp[$i] = array();
-//	for ($j = 1; $j < 1000; $j++) {
-//		if (isset($codes[$i][$j])) {
-//			$tmp[$i][$j] = $codes[$i][$j];
-//		}
-//	}
-//}
-//
-//echo '<pre>';
-//var_export($tmp);
-//exit;
 /**
  * PHP version check
  */
