097521ef8c179239305e35e0bb416eef06d62159
Gabor Ivan <gabor.ivan@westwing.de>
2015-04-22 18:08:03 +0200

    2nd UnitTest: not ready

--- wh_application/module/WebHemi2/test/WebHemi2Test/Controller/UserControllerTest.php
+++ wh_application/module/WebHemi2/test/WebHemi2Test/Controller/UserControllerTest.php
@@ -27,18 +27,19 @@
 namespace WebHemi2Test\Controller;
 
 use WebHemi2Test\Bootstrap;
+use WebHemi2Test\Fixture\AuthenticatedUserFixture;
 use Zend\Mvc\Router\Http\TreeRouteStack as HttpRouter;
 use Zend\Http\Request;
 use Zend\Http\Response;
 use Zend\Mvc\MvcEvent;
 use Zend\Mvc\Router\RouteMatch;
 use PHPUnit_Framework_TestCase;
-use WebHemi2\Controller\WebsiteController;
+use WebHemi2\Controller\UserController;
 
 /**
  * WebHemi2
  *
- * Website Controller Test
+ * User Controller Test
  *
  * @category  WebHemi2Test
  * @package   WebHemi2Test_Controller
@@ -47,7 +48,7 @@ use WebHemi2\Controller\WebsiteController;
  * @license   http://webhemi.gixx-web.com/license/new-bsd   New BSD License
  * @link      http://www.gixx-web.com
  */
-class WebsiteControllerTest extends PHPUnit_Framework_TestCase
+class UserControllerTest extends PHPUnit_Framework_TestCase
 {
     /** @var  WebsiteController $controller */
     protected $controller;
@@ -67,9 +68,9 @@ class WebsiteControllerTest extends PHPUnit_Framework_TestCase
     {
         $serviceManager = Bootstrap::getServiceManager();
 
-        $this->controller = new WebsiteController();
+        $this->controller = new UserController();
         $this->request    = new Request();
-        $this->routeMatch = new RouteMatch(array('controller' => 'Website'));
+        $this->routeMatch = new RouteMatch(array('controller' => 'User'));
         $this->event      = new MvcEvent();
         $config = $serviceManager->get('Config');
         $routerConfig = isset($config['router']) ? $config['router'] : array();
@@ -84,13 +85,40 @@ class WebsiteControllerTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * Test if the controller can be accessed
+     * Test if the controller can be accessed without login
      */
-    public function testIndexActionCanBeAccessed()
+    public function testUserProfileActionCanBeAccessedWithoutLogin()
     {
-        $this->routeMatch->setParam('action', 'index');
+        // Without login you should not have access and get redirected
+        $this->routeMatch->setParam('action', 'userProfile');
 
-        $result   = $this->controller->dispatch($this->request);
+        $result = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertInstanceOf('Zend\Http\PhpEnvironment\Response', $result);
+        $this->assertEquals(302, $response->getStatusCode());
+    }
+
+    /**
+     * Test if the controller can be accessed WITH login
+     */
+    public function testUserProfileActionCanBeAccessedWithLogin()
+    {
+        $this->routeMatch->setParam('action', 'userProfile');
+
+        $mockedController = $this->getMockBuilder('UserController')
+            ->getMock();
+
+        $loggedInUserAuth = new AuthenticatedUserFixture();
+        $mockedController->method('getUserAuth')
+            ->willReturn($loggedInUserAuth);
+
+        $mockedController->setEvent($this->event);
+        $mockedController->setServiceLocator(Bootstrap::getServiceManager());
+        $mockedController->getPluginManager()
+            ->setInvokableClass('UserAuth', 'WebHemi2\Controller\Plugin\UserAuth');
+
+        $result = $this->controller->dispatch($this->request);
         $response = $this->controller->getResponse();
 
         $this->assertInternalType('array', $result);
---
+++ wh_application/module/WebHemi2/test/WebHemi2Test/Fixture/AuthenticatedUserFixture.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * WebHemi2
+ *
+ * PHP version 5.4
+ *
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://webhemi.gixx-web.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@gixx-web.com so we can send you a copy immediately.
+ *
+ * @category  WebHemi2Test
+ * @package   WebHemi2Test
+ * @author    Gabor Ivan <gixx@gixx-web.com>
+ * @copyright 2015 Gixx-web (http://www.gixx-web.com)
+ * @license   http://webhemi.gixx-web.com/license/new-bsd   New BSD License
+ * @link      http://www.gixx-web.com
+ */
+namespace webHemi2Test\Fixture;
+
+/**
+ * Authentication fixture for UnitTest
+ *
+ * @category  WebHemi2Test
+ * @package   WebHemi2Test
+ * @author    Gabor Ivan <gixx@gixx-web.com>
+ * @copyright 2015 Gixx-web (http://www.gixx-web.com)
+ * @license   http://webhemi.gixx-web.com/license/new-bsd   New BSD License
+ * @link      http://www.gixx-web.com
+ */
+class AuthenticatedUserFixture
+{
+    /**
+     * Proxy convenience method
+     *
+     * @return bool
+     */
+    public function hasIdentity()
+    {
+        return true;
+    }
+}
